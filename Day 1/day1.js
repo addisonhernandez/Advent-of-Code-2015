26b109c3f3261e6add0f60b96a06f37a26ab172b
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-03 15:52:47 -0500

    Add solution to Part One

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -7,6 +7,29 @@ const day = 1;
 const session = process.env.AOC_SESSION;
 
 aocLoader(year, day, session).then(function (data) {
-  console.log(data);
+  const finalFloor = parseFloors(data);
+
+  console.log(`Final Floor: ${finalFloor}`)
 });
 
+/**
+ * Parses a string of directions to an apartment in a very tall building.
+ * `(` means go up one floor.
+ * `)` means go down one floor.
+ * Santa starts on floor 0.
+ * @param {string} parens - A string of parentheses giving Santa directions.
+ * @returns {number} The floor that the directions end on.
+ */
+const parseFloors = function (parens) {
+  let currentFloor = 0;
+
+  for (const paren of parens) {
+    if (paren === '(') { 
+      currentFloor++;
+    } else if (paren === ')') {
+      currentFloor--;
+    }
+  }
+
+  return currentFloor;
+}
