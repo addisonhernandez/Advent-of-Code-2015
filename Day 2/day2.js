336e24bd2d412102fb899dc2da260614faef19b0
Addison Hernandez <addison.hernandez@gmail.com>
2022-02-11 22:46:41 -0500

    Add solution for Day 2 Part 2

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -7,23 +7,34 @@ function main() {
   const session = process.env.AOC_SESSION;
 
   aocLoader(year, day, session).then(function (input) {
-    // console.log(data);
-    const boxes = input.split('\n');
+    const boxesDimensions = (
+      input
+        .split('\n')
+        // Each box has dimensions `LxWxH`
+        .map((LxWxH) => parseDimensions(LxWxH))
+    );
 
-    const totalPaperArea = boxes.reduce((total, box) => {
-      // Each box has dimensions `LxWxH`
-      const [L, W, H] = parseDimensions(box);
-
+    const totalPaperArea = boxesDimensions.reduce((totalArea, [L, W, H]) => {
       const LW = L * W;
       const WH = W * H;
       const HL = H * L;
 
       const slack = Math.min(LW, WH, HL);
 
-      return total + 2 * (LW + WH + HL) + slack;
+      return totalArea + 2 * (LW + WH + HL) + slack;
+    }, 0);
+
+    const totalRibbonLength = boxesDimensions.reduce((totalLength, [L, W, H]) => {
+      const bowLength = L * W * H;
+      const ribbonLength = (L === Math.max(L, W, H)) ?
+        2 * (W + H) :
+        2 * (L + Math.min(W, H));
+
+      return totalLength + bowLength + ribbonLength;
     }, 0);
 
     console.log(`Total sqft of wrapping paper: ${totalPaperArea}`);
+    console.log(`Total length of ribbon: ${totalRibbonLength}`);
   });
 }
 
